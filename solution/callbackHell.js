18a53c688e8f6adfc49789a63b344d06dcec23ad
Andrey Savinovskikh <andrey.savinovskikh@gmail.com>
2021-10-15 04:07:27 +0700

    улучшено решение на колбеках

--- solution/callbackHell.js
+++ solution/callbackHell.js
@@ -1,28 +1,24 @@
 module.exports = function (Homework) {
+
+  function iter(i, array, acc, length, fn, cb) {
+    array.get(i, (curr) => {
+      fn(acc, curr, i, array, (newAcc) => {
+        Homework.add(i, 1, (newI) => {
+          Homework.less(newI, length, (isLess) => {
+            if (isLess) {
+              iter(newI, array, newAcc, length, fn, cb);
+            } else {
+              cb(newAcc);
+            }
+          })
+        });
+      });
+    })
+  }
+
   return async (array, fn, initialValue, cb) => {
-    let i = 0,
-        acc = initialValue;
-
     array.length((length) => {
-      (function get() {
-        array.get(i, (curr) => {
-          fn(acc, curr, i, array, (newAcc) => {
-            acc = newAcc;
-
-            Homework.add(i, 1, (newI) => {
-              i = newI;
-
-              Homework.less(newI, length, (isLess) => {
-                if (isLess) {
-                  get();
-                } else {
-                  cb(acc);
-                }
-              })
-            });
-          });
-        })
-      })()
+      iter(0, array, initialValue, length, fn, cb)
     })
   }
 }
